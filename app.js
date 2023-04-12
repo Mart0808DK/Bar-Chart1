9ee714ca2fe70147e3077fe0988cd66bf3ca809b
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-12 11:21:08 +0200

    Ændret på den første søjle i js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 "use strict";
 
+window.addEventListener("load", start) 
+
+function start() {
+    console.log("Javascript Kører");
+
+    const bars = document.querySelectorAll(".bar");
+
+    const firstBar = bars[0];
+
+    firstBar.style.height = "100px";
+
+
+}
