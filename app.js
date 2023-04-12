6a232490dfdbd613262467275fd3cb3c58959dda
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-12 11:43:58 +0200

    displayData funktion er færdig

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ const numbers = [12, 5, 9, 33, 16, 4, 21, 26, 17, 24, 0, 11, 31, 22, 7, 6, 29, 3
 function start() {
     console.log("Javascript Kører");
 
-    const bars = document.querySelectorAll(".bar");
-
-    const firstBar = bars[0];
-
-    const h = numbers[0] / 32 *100
-
-
-    firstBar.style.height = `${h}px`;
+    displayData();
 
 
 }
 
+function displayData() {
+    const bars = document.querySelectorAll(".bar");
+
+    
+    for (let i = 0; i < 40; i++) {
+        const wholeBar = bars[i];
+    
+        const h = (numbers[i] / 32) * 100;
+    
+        wholeBar.style.height = `${h}px`;
+        
+    }
+}
