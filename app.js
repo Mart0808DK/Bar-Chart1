838cd6084200bd0753c7c40b705fc32d0e88a671
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-13 09:17:35 +0200

    færdig project
    funktion der sætter tilfældig værdier og funktioner der tilføjer og fjerner i array og noget der opdatere det hvert sek

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 
 window.addEventListener("load", start) 
 
-const numbers = [12, 5, 9, 33, 16, 4, 21, 26, 17, 24, 0, 11, 31, 22, 7, 6, 29, 3, 8, 13, 20, 28, 18, 10, 23, 14, 15, 27, 1, 19, 32, 2, 25, 30];
+const numbers = [12, 5, 9, 33, 16, 4, 21, 26, 17, 24, 0, 11, 31, 22, 7, 6, 29, 3, 8, 13, 20, 28, 18, 10, 23, 14, 15, 27, 1, 19, 32, 2, 25, 30,36,21,20,2,16];
 
 function start() {
     console.log("Javascript Kører");
-
-    displayData();
+    setInterval(setBarChart, 1000);
+    setBarChart();
+    
 
 
 }
@@ -24,4 +25,19 @@ function displayData() {
         wholeBar.style.height = `${h}px`;
         
     }
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function updateQueueArray() {
+  numbers.unshift(randomInt(0, 32));
+  numbers.pop();
+}
+
+
+function setBarChart() {
+    displayData();
+    updateQueueArray();
 }
